b9c1c29fbf8b033746d041f01d941b008f2cb220
B533P-08\533 <pluc113@hotmail.com>
2018-10-13 11:31:29 -0400

    pratique pour l'examen

---
+++ pratique_examen/pratique_examen/Assets/script/questionBang/BangLifeTime.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// gameObject = dans l'objet ou se trouve script / et l'autre GameObject= type
+
+public class BangLifeTime : MonoBehaviour {
+    [SerializeField] float Temps = 2;
+    [SerializeField] Text TextBang;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Temps <= 0)
+        {
+            Destroy(gameObject);
+        }
+        else if(Temps > 0)
+        {
+            Temps -= Time.deltaTime;
+            changerCouleur();
+        }
+
+	}
+
+    void changerCouleur()
+    {
+        TextBang.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+    }
+}
---
+++ pratique_examen/pratique_examen/Assets/script/questionBang/scriptBang.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptBang : MonoBehaviour {
+    [SerializeField] public Canvas Bang;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Jump") > 0)
+        {
+            Instantiate(Bang);
+        }
+
+    }
+}
